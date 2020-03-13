b8d7e0ce523de0bbc7778f558eaa25f4cd7c2c0c
alanbanks229 <alanbanks229@gmail.com>
2020-03-13 16:09:08 -0400

    We donezo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,34 @@ document.addEventListener('DOMContentLoaded', pageSetup);
 function pageSetup() {
 	console.log('connected');
 	getDoggos();
+	let filterBtn = document.querySelector('#good-dog-filter')
+	filterBtn.filter = false;
+	filterBtn.addEventListener('click', getFilteredDoggos)
+}
+
+function filterDoggos(dogData, filter){
+	console.log(dogData)
+	let bar = document.querySelector('#dog-bar')
+	bar.innerHTML = ""
+	if (filter){
+		let filteredArr = dogData.filter( dog => dog.isGoodDog === true )
+		filteredArr.forEach( dog => makeDogButton(dog))
+	} else {
+		getDoggos()
+	}
+}
+
+function getFilteredDoggos(e){
+
+	let target = e.target
+	if (target.innerText === "Filter good dogs: OFF"){
+		target.innerText = "Filter good dogs: ON"
+	} else {
+		target.innerText = "Filter good dogs: OFF"
+	}
+	e.target.filter = !e.target.filter
+	fetch('http://localhost:3000/pups').then((response) => response.json()).then((json) => filterDoggos(json, e.target.filter));
+	//console.log(e.target.filter)
 }
 
 function getDoggos() {
@@ -58,6 +86,14 @@ function buttonControl(btn) {
 		btn.target.good = true;
 		btn.target.innerText = 'Make this Dog Bad';
 	}
+	let dogId = btn.target.dogId
+	fetch(`http://localhost:3000/pups/${dogId}`, {
+		method: "PATCH",
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({isGoodDog: btn.target.good})
+	})
 	console.log(btn.target.dogId);
 	console.log(btn.target.good);
 }
